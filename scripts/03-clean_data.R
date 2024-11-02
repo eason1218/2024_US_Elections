3cfcd0fa30fa0c4af7dae381e2114c90ae8ba588
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-11-02 00:17:36 -0400

    Update 03-clean_data.R

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -9,36 +9,19 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(dplyr)
+library(readr)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+poll <- read.csv("president_polls.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+poll_of_polls <- poll %>%
+  group_by(pollster, answer) %>%
+  summarize(count = n()) %>%
+  ungroup() %>%
+  group_by(pollster) %>%
+  mutate(percentage = count / sum(count) * 100) %>%
+  select(pollster, answer, percentage)
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(poll_of_polls, "data/02-analysis_data/analysis_data.csv")
